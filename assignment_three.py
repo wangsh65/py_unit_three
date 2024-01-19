0a682d8813e0f184c82299477ec2174133a3a816
wangsh65 <144366234+wangsh65@users.noreply.github.com>
2024-01-19 15:41:12 -0500

    Update assignment_three.py
    Can you help me to check my correction. Thank you !

--- assignment_three.py
+++ assignment_three.py
@@ -1,30 +1,33 @@
 #Shelly Wang
 #Oct 17,2023
 #purpose: to get a prism’s total surface area
+#last correction: 1/19/2024
 
 
 def printInstructions():
     """this function takes no paramters are prints out instructions to the users;returns:none"""
-    print("This program computes the surface area of a rectangular prism")
     print("please input the length, width, and height of the solid")
-# create the total function
+    print("This program computes the surface area of a rectangular prism")
+
+
+
 def areaRectangle(side1, side2):
     rectangle_area = side1*side2
-    return rectangle_area
-# Use def to figure out the area of one side and then go back
-def totalSurfaceArea(l,w,h):
+    return rectangle_area                                                                   # use return to invoke areaRactangle
+                                                                                                # Use def to figure out the area of one side and then go back
+def totalSurfaceArea(l,w,h):                                                                    # to make a new function
 
     areaRectangle(l, w)
     areaRectangle(l, h)
     areaRectangle(w, h)
 
     area=int(areaRectangle(l, w))*2+int(areaRectangle(l, h))*2+int(areaRectangle(w, h))*2
-    return area
-#  You figure out the three sides, and then you multiply by 2 and that's its surface area
+    return area                                                                                 # use return to invoke area, and it can to make the function correct
+                                                                                                #  You figure out the three sides, and then you multiply by 2 and that's its surface area
 def printResult(result):
     print("THe surface area is",result)
-# 'print' to get the answer
-def ask_length():
+                                                                                                # 'print' to get the answer
+def ask_length():                                                                               # to make a new function ask a length, width and height
     l = int(input("What is the length: "))
     return l
 
@@ -35,7 +38,7 @@ def ask_width():
 def ask_height():
     h = int(input("What is the height: "))
     return h
-# to ask user how many length, width and height do there have?
+                                                                                                # to ask user how many length, width and height do there have?
 def main():
     printInstructions()
     l = ask_length()
@@ -43,7 +46,7 @@ def main():
     h = ask_height()
     result = totalSurfaceArea(l, w, h)
     printResult(result)
-#  To call all the previous ones function
+                                                                                                #  To call all the previous ones function
 main()
 
 #test
